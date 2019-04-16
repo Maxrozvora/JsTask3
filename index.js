50a1cd5b02d3b6375691d3f42acccdfded9d84a9
Maxrozvora <rozvora.max@gmail.com>
2019-04-16 23:06:32 +0300

    index.js смена табуляции

--- index.js
+++ index.js
@@ -1,81 +1,82 @@
 const cities = ['Київ', 'Харків', 'Львів', 'Одеса', 'Дніпро'];
 
 const distances = [
-  [0,200,250,300,350],
-  [200,250,300,350,0],
-  [250,300,350,0,200],
-  [300,350,0,200,250],
-  [350,0,200,250,300]
+    [0, 200, 250, 300, 350],
+    [200, 250, 300, 350, 0],
+    [250, 300, 350, 0, 200],
+    [300, 350, 0, 200, 250],
+    [350, 0, 200, 250, 300]
 ];
 
-const dayOfWeek = [	"Неділя","Понеділок","Вівторок","Середа","Четвер","П'ятниця","Субота"];
+const dayOfWeek = ["Неділя", "Понеділок", "Вівторок", "Середа", "Четвер", "П'ятниця", "Субота"];
 
 
 const data = [];
 
 
 const schredule = {
-  getArriveCity() {
-    for (let city in cities) { 
-      for (departures in cities) {
-        if (cities[city] == cities[departures]) {
-          continue
+    getArriveCity() {
+        for (let city in cities) {
+            for (departures in cities) {
+                if (cities[city] == cities[departures]) {
+                    continue
+                }
+
+                const date = new Date(this.randomInteger(new Date().getTime(), new Date().getTime() + 604800000));
+
+                const day = date.getDay();
+                console.log(date.getDate()); // TODO console.log
+
+                const time = `${date.getHours()}:${date.getMinutes()}`;
+
+                console.log(date.toLocaleTimeString()); // TODO console.log
+
+                let obj = {
+                    'from': cities[city],
+                    'to': cities[departures],
+                    'number': this.getNumberOfTrain(),
+                    'day': dayOfWeek[date.getDay()],
+                    'departure': {
+                        'day': this.getDayOfWeek(date),
+                        'time': date.toLocaleTimeString()
+                    },
+                };
+
+                data.push(obj)
+            }
         }
+    },
 
-        const date = new Date(this.randomInteger(new Date().getTime(), new Date().getTime() + 604800000));
+    getNumberOfTrain() {
+        return Math.floor(Math.random() * (999 - 100 + 1) + 100) + Math.random().toString(36).substring(12, 15).toUpperCase();
+    },
 
-        const day = date.getDay();
-        console.log(date.getDate()); // TODO console.log
-       
-        const time = `${date.getHours()}:${date.getMinutes()}`;
+    randomInteger(min, max) {
+        var rand = min - 0.5 + Math.random() * (max - min + 1)
+        rand = Math.round(rand);
+        return rand;
+    },
 
-        console.log(date.toLocaleTimeString()); // TODO console.log
-        
-        let obj = {
-          'from': cities[city],
-          'to': cities[departures],
-          'number': this.getNumberOfTrain(),
-          'day': dayOfWeek[date.getDay()],
-          'departure': {
-            'day':this.getDayOfWeek(date),
-            'time':date.toLocaleTimeString()
-          },
-        };
+    getDayOfWeek(date) {
+        if (date.getDate() === new Date().getDate()) {
+            return 'Сьгодні'
+        } else if (date.getDate() === new Date().getDate() + 1) {
+            return 'Завтра'
+        } else {
+            return dayOfWeek[date.getDay()]
+        }
+    },
 
-        data.push(obj)
-      }
-    }
-  },
 
-  getNumberOfTrain() {
-    return Math.floor(Math.random()*(999-100+1)+100) + Math.random().toString(36).substring(12, 15).toUpperCase();
-  },
+    getTime(date) {
 
-  randomInteger(min, max) {
-  var rand = min - 0.5 + Math.random() * (max - min + 1)
-  rand = Math.round(rand);
-  return rand;
-},
+    },
 
-  getDayOfWeek(date) {
-    if (date.getDate() === new Date().getDate()) {
-      return 'Сьгодні'
-    } else if (date.getDate() === new Date().getDate() + 1) {
-      return 'Завтра'
-    } else {
-      return dayOfWeek[date.getDay()]
-    }
-  },
-
-  getTime(date) {
-
-  },
-  
-  renderTable(data) {
-    let tr;
-    for(let item of data) {
-      console.log(item);
-      const itemRow = `<tr>
+    renderTable(data) {
+        let tr;
+        for (let item of data) {
+            console.log(item);
+            const itemRow = `<tr>
       <td>${item.number}</td>
       <td>${item.from}</td>
       <td>${item.to}</td>
@@ -87,13 +88,13 @@ const schredule = {
       <td></td>
       <td></td>
     </tr>`
-    
-    tr = tr + itemRow;
-    
-  }
-  
-  document.querySelector('tbody').innerHTML = tr
-  }
+
+            tr = tr + itemRow;
+
+        }
+
+        document.querySelector('tbody').innerHTML = tr
+    }
 };
 
 schredule.getArriveCity();
